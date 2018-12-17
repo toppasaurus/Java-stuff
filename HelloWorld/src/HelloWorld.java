8c7f3d5dc0b4c1d1696b9546802454f1abb64cf7
Tim Warner <renrawmit@gmail.com>
2018-12-17 12:31:50 -0500

    Now to try syncing. Push then pull, yeah?

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
 		System.out.println("You kinda suck sometimes!");
+		System.out.println("Seriously. You do.");
 
 	}
 
